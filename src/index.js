756a17efb0f005ddff4080b99b8bc7b7408d0188
John Lindquist <johnlindquist@gmail.com>
2019-01-21 10:54:28 -0700

    handler.set

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 "use strict"
 
-let person = {}
-Object.seal(person)
-Reflect.set(person, "name", "John") /*?*/
+let person = {
+  name: "John"
+}
 
+let handler = {
+  set(target, key, value) {
+    console.log(`${key}: ${target[key]} -> ${value}`)
+    return Reflect.set(target, key, value)
+  }
+}
+
+person = new Proxy(person, handler)
+
+person.name = "Mindy"
+person.name = "Ben"
 console.log(person.name)
